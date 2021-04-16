3f71dbb3b63943733b83271f4c9b28a6db8dc5b9
shasiyizhizhigengniao1775 <76928317+shasiyizhizhigengniao1775@users.noreply.github.com>
2021-04-16 21:30:37 +0800

    Add files via upload
    页面

---
+++ center_maintain_evaluate.vue
@@ -0,0 +1,162 @@
+<template>
+	<view class="pag">
+		<view class="bg">
+			<view class="head">
+				<u-icon name="arrow-left" style="padding-left:30rpx;" color="#fff" size="36"></u-icon>
+				<view class="title">
+					个人评价
+				</view>
+			</view>
+			<view class="per">
+				<u-avatar size='large' :src="src"></u-avatar>
+				<view class="name">
+					小福子
+				</view>
+			</view>
+		</view>
+		<view>
+			<view class="point">
+				客户评价{{value}}分
+			</view>
+			<u-rate size="64" gutter="45" active-color="#ffd700" inactive-color="#b2b2b2" :count="count" v-model="value" style="margin: 50rpx 100rpx;"></u-rate>
+		</view>
+		<view class="log" v-for="(item,index) in list" :key="item.content">
+			<u-tag border-color="#fff" class="mini-log" @click="changeLog(index)" :bg-color="item.bgcolor" :color="item.color"
+			 :text="item.content" shape="circle" type="success" mode="light" />
+		</view>
+
+	</view>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+				src:'https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg',//头像
+				count: 5,
+				value: 2,
+				list: [{
+						content: '着装整齐',
+						bgcolor: '',
+						color: '#228d22',
+						active: ''
+					},
+					{
+						content: '服务态度好',
+						bgcolor: '#1daa22',
+						color: '#fff',
+						active: 'true'
+					},
+					{
+						content: '负责任很有耐心',
+						bgcolor: '',
+						color: '#228d22',
+						active: ''
+					},
+					{
+						content: '手法很专业',
+						bgcolor: '',
+						color: '#228d22',
+						active: ''
+					},
+					{
+						content: '场地整洁',
+						bgcolor: '',
+						color: '#228d22',
+						active: ''
+					},
+					{
+						content: '很耐心',
+						bgcolor: '',
+						color: '#228d22',
+						active: ''
+					},
+					{
+						content: '服务态度很好',
+						bgcolor: '#1daa22',
+						color: '#fff',
+						active: 'true'
+					},
+					{
+						content: '负责任有耐心',
+						bgcolor: '',
+						color: '#228d22',
+						active: ''
+					},
+				]
+			}
+		},
+		methods: {
+			changeLog(index) {
+				if (this.list[index].active == '') {
+					this.list[index].bgcolor = '#1daa22';
+					this.list[index].color = '#fff';
+					this.list[index].active = '#true';
+
+				} else {
+					this.list[index].bgcolor = '';
+					this.list[index].color = '#228d22';
+					this.list[index].active = '';
+				}
+
+			}
+		}
+	}
+</script>
+
+<style>
+	.pag {
+		/* position: relative; */
+		width: 100%;
+		height: 1334rpx;
+		background: #fff;
+	}
+
+	.bg {
+		width: 100%;
+		height: 430rpx;
+		background: url(https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg);
+	}
+
+	.head {
+		display: flex;
+		padding-top: 65rpx;
+	}
+
+	.title {
+		color: #fff;
+		font-size: medium;
+		padding-left: 250rpx;
+	}
+
+	.per {
+		position: absolute;
+		top: 140rpx;
+		left: 310rpx;
+	}
+
+	.name {
+		text-align: center;
+		margin-top: 15rpx;
+		color: #fff;
+		font-size: small;
+	}
+
+	.info {
+		margin: 20rpx 35rpx;
+	}
+
+	.point {
+		font-size: medium;
+		text-align: center;
+		margin-top: 85rpx;
+	}
+
+	.log {
+		margin: 20rpx 70rpx;
+	}
+
+	.mini-log {
+		float: left;
+		margin: 13rpx 10rpx;
+	}
+</style>
---
+++ center_user.vue
@@ -0,0 +1,169 @@
+<template>
+	<view class="pag">
+		<view class="bg">
+			<view class="head">
+				<u-icon name="arrow-left" style="padding-left:30rpx;" color="#fff" size="36"></u-icon>
+				<view class="title">
+					个人中心
+				</view>
+			</view>
+			<view class="per">
+				<u-avatar size='large' :src="src"></u-avatar>
+				<view class="infor">
+					<view class="infor1">186******5976</view>
+					<view class="infor2">轴承区域建北小区20号楼4单元201室</view>
+				</view>
+			</view>
+		</view>
+
+		<view class="but-list">
+			<view class="but">
+				<u-image :src="require('images/centerorder.png')" height="60rpx" width="70rpx" style="margin-left: 15rpx;"></u-image>
+				<view class="but-t">我的订单</view>
+			</view>
+			<view class="but">
+				<u-image :src="require('images/centermaintain.png')" height="60rpx" width="70rpx"style="margin-left: 38rpx;"></u-image>
+				<u-button id="but-center" type="success" shape="circle" size="mini" >我要维护</u-button>
+			</view>
+			<view class="but">
+				<u-image :src="require('images/centerdatum.png')" height="60rpx" width="80rpx"style="margin-left: 18rpx;"></u-image>
+				<view class="but-t">个人资料</view>
+			</view>
+		</view>
+
+		<view class="pass-list">
+			<view class="pass">
+				<u-image :src="require('images/centerpass.png')" style="float: left; margin-right: 50rpx;" height="50rpx" width="44rpx"></u-image>
+				<view class="but-text1" style="padding-bottom: 30rpx; ">修改密码</view>
+			</view>
+			<view class="message">
+				<u-image :src="require('images/centerinfor.png')" style="margin-right: 50rpx;" height="35rpx" width="40rpx"></u-image>
+				<view class="but-text2" style="padding-bottom: 20rpx; ">我的消息</view>
+			</view>
+		</view>
+
+		<u-button type="success" style="margin:460rpx 20rpx;">退出登录</u-button>
+
+	</view>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+src:'https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg',//头像
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.pag {
+		width: 100%;
+		height: 1334rpx;
+		background: #eee;
+	}
+
+	.bg {
+		width: 100%;
+		height: 430rpx;
+		background: url(https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg);
+	}
+
+	.head {
+		display: flex;
+		padding-top: 65rpx;
+
+	}
+
+	.per {
+		display: flex;
+		position: absolute;
+		top: 190rpx;
+		left: 60rpx;
+	}
+
+	.info {
+		margin: 20rpx 35rpx;
+	}
+
+	.infor1 {
+		margin-bottom: 13rpx;
+		color: #fff;
+		font-size: medium;
+	}
+
+	.infor2 {
+		color: #fff;
+		font-size:small;
+	}
+
+	.title {
+		color: #fff;
+		font-size: medium;
+		padding-left: 250rpx;
+	}
+
+	.but-list {
+		position: absolute;
+		top: 350rpx;
+		left: 20rpx;
+		width: 710rpx;
+		height: 250rpx;
+		background-color: #fff;
+		border-radius: 10px;
+		display: flex;
+		justify-content: space-around;
+	}
+
+	.but {
+		margin-top: 60rpx;
+	}
+
+	#but-center {
+		font-size: medium;
+		margin-top: 15rpx;
+	}
+
+	.but-t {
+		margin-top: 19rpx;
+		font-size: medium;
+	}
+
+	.but-text1 {
+		position: absolute;
+		top: 50rpx;
+		left: 130rpx;
+	}
+
+	.but-text2 {
+		position: absolute;
+		top: 150rpx;
+		left: 130rpx;
+	}
+
+	.pass-list {
+		background: #fff;
+		position: absolute;
+		top: 625rpx;
+		left: 20rpx;
+		width: 710rpx;
+		height: 230rpx;
+		border-radius: 10px;
+		padding: 0 50rpx;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.pass {
+		border-bottom: #ccc 1rpx solid;
+		height: 50%;
+		padding-top: 40rpx;
+	}
+
+	.message {
+		padding-top: 40rpx;
+	}
+</style>
